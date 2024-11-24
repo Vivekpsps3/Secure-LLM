0b7bbc041eb5ede30db3bba89ccfb8f887d30af4
ihagedo <ihagedo@purdue.edu>
2024-11-24 18:03:00 -0500

    added rsa variable client key creation

--- rsa_keypair.py
+++ rsa_keypair.py
@@ -1,8 +1,8 @@
 from Crypto.PublicKey import RSA
+import sys
 
 # Generate a 4096-bit RSA key pair
 key = RSA.generate(4096)
-key2 = RSA.generate(4096)
 
 # Save the private for the server
 with open("server.priv", "wb") as f:
@@ -12,11 +12,16 @@ with open("server.priv", "wb") as f:
 with open("server.pub", "wb") as f:
     f.write(key.publickey().export_key())
 
-# Save the private for the client
-with open("client.priv", "wb") as f:
-    f.write(key2.export_key())
+clients = 3
+if len(sys.argv) > 1:
+    clients = int(sys.argv[1])
+for i in range(clients):
+    key = RSA.generate(4096)
+    # Save the private for the client
+    with open(f"client{i}.priv", "wb") as f:
+        f.write(key.export_key())
 
-# Save the public key for the client
-with open("client.pub", "wb") as f:
-    f.write(key2.publickey().export_key())
+    # Save the public key for the client
+    with open(f"client{i}.pub", "wb") as f:
+        f.write(key.publickey().export_key())
 
